fb4d40f4b8034a65cf5fa23118155996f98183d7
oakes <zsoakes@gmail.com>
2015-07-23 21:58:08 -0400

    Create list of tokens so we know where to add markup

--- src/cljs/paren_soup/core.cljs
+++ src/cljs/paren_soup/core.cljs
@@ -1,31 +1,52 @@
 (ns paren-soup.core
   (:require [cljs.tools.reader :refer [read]]
             [cljs.tools.reader.reader-types :refer [indexing-push-back-reader]]
-            [schema.core :refer [maybe Any Str Keyword]])
+            [clojure.walk :refer [postwalk]]
+            [schema.core :refer [maybe either Any Str Keyword]])
   (:require-macros [schema.core :refer [defn with-fn-validation]]))
 
-(defn read-safe :- (maybe {Keyword Any})
-  "Returns a map containing either a form or an exception, along with the line
-and column numbers, or nil if EOF is reached."
+(def read-result (either [Any] js/Error))
+
+(defn read-safe :- (maybe read-result)
+  "Returns either a form or an exception object, or nil if EOF is reached."
   [reader :- js/Object]
   (try
-    (when-let [form (read reader false nil)]
-      (assoc (meta form) :form form))
-    (catch js/Object e
-      (assoc (.-data e) :message (.-message e)))))
+    (read reader false nil)
+    (catch js/Error e e)))
 
-(defn read-all :- [{Keyword Any}]
-  "Returns a list of maps representing each top-level form."
+(defn read-all :- [read-result]
+  "Returns a list of values representing each top-level form."
   [s :- Str]
   (let [reader (indexing-push-back-reader s)]
     (take-while some? (repeatedly (partial read-safe reader)))))
 
+(defn token-list :- [{Keyword Any}]
+  "Returns a list of maps describing each token."
+  ([token :- Any]
+    (token-list token []))
+  ([token :- Any
+    result :- [Any]]
+    (flatten
+      (conj result
+            (if (instance? js/Error token)
+              (assoc (.-data token) :message (.-message token))
+              (if (coll? token)
+                [(assoc (meta token) :type (type token))
+                 (map token-list token)]
+                (assoc (meta token) :type (type token))))))))
+
+(defn add-markup :- Str
+  "Returns a copy of the given string with markup added."
+  [s :- Str]
+  (let [tokens (read-all s)]
+    (pr-str (map token-list tokens))))
+
 (defn init! []
   (let [editor (.querySelector js/document ".paren-soup")]
     (set! (.-spellcheck editor) false)
-    (.log js/console (pr-str (read-all (.-value editor))))))
+    (.log js/console (add-markup (.-value editor)))))
 
 (defn init-with-validation! []
   (with-fn-validation (init!)))
 
-(init!)
+#_(init!)
