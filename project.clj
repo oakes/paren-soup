503bd66221425a74557e57e8ea6c260981d54cf0
oakes <zsoakes@gmail.com>
2015-08-06 10:50:54 -0400

    Update ClojureScript

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "FIXME: write this!"
   :dependencies [[org.clojars.oakes/tools.reader "0.10.1-SNAPSHOT"]
                  [org.clojure/clojure "1.7.0"]
-                 [org.clojure/clojurescript "1.7.28"]
+                 [org.clojure/clojurescript "1.7.48"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [prismatic/schema "0.4.3"]
                  [ring "1.4.0"]]
