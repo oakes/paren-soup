a3b3d7942cd153ae750dde95d69fa65c286aa6de
oakes <zsoakes@gmail.com>
2016-06-15 18:50:35 -0400

    Start 2.0.13-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paren-soup "2.0.12"
+(defproject paren-soup "2.0.13-SNAPSHOT"
   :description "A viewer and editor for ClojureScript"
   :url "https://github.com/oakes/paren-soup"
   :license {:name "Public Domain"
