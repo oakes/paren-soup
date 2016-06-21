ed2a947646a3bef8fa5cea8dea51b53367bee6fd
oakes <zsoakes@gmail.com>
2016-06-21 18:13:00 -0400

    Increment version number

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject paren-soup "2.1.1-SNAPSHOT"
+(defproject paren-soup "2.1.1"
   :description "A viewer and editor for ClojureScript"
   :url "https://github.com/oakes/paren-soup"
   :license {:name "Public Domain"
