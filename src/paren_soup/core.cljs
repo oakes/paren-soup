168148255ec20be2cc14c745b617807de214fd5e
oakes <zach@sekao.net>
2019-01-19 16:07:24 -0600

    Only set cursor position if element is active

--- src/paren_soup/core.cljs
+++ src/paren_soup/core.cljs
@@ -115,11 +115,12 @@
   "Does additional work on the content after it is rendered."
   [content events-chan {:keys [cropped-state] :as state}]
   ; set the cursor position
-  (if (some->> cropped-state :element (.contains content))
-    (some-> cropped-state :element (dom/set-cursor-position! (:cursor-position cropped-state)))
-    (if (and (:selection-change? state) (:original-cursor-position state))
-      (dom/set-cursor-position! content (:original-cursor-position state))
-      (dom/set-cursor-position! content (:cursor-position state))))
+  (when (= content js/document.activeElement)
+    (if (some->> cropped-state :element (.contains content))
+      (some-> cropped-state :element (dom/set-cursor-position! (:cursor-position cropped-state)))
+      (if (and (:selection-change? state) (:original-cursor-position state))
+        (dom/set-cursor-position! content (:original-cursor-position state))
+        (dom/set-cursor-position! content (:cursor-position state)))))
   ; set up errors
   (hide-error-messages! (.-parentElement content))
   (doseq [elem (-> content (.querySelectorAll ".error") array-seq)]
