cdda79220d0680a5f735ecd857332233ef921378
oakes <zach@sekao.net>
2019-02-24 11:49:13 -0600

    Enable expound during dev

--- src/paren_soup/dev.cljs
+++ src/paren_soup/dev.cljs
@@ -1,5 +1,8 @@
 (ns paren-soup.dev
   (:require [paren-soup.core]
-            [orchestra-cljs.spec.test :as st]))
+            [orchestra-cljs.spec.test :as st]
+            [expound.alpha :as expound]
+            [clojure.spec.alpha :as s]))
 
 (st/instrument)
+(set! s/*explain-out* expound/printer)
