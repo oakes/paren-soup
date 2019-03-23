21cb2a35724891957c4357c88d5294b6896fd53d
oakes <zach@sekao.net>
2019-03-23 14:55:16 -0600

    Improve read-deps-edn fn and change target dir

--- prod.clj
+++ prod.clj
@@ -1,7 +1,8 @@
 (require
+  '[clojure.java.io :as io]
+  '[clojure.string :as str]
   '[cljs.build.api :as api]
   '[leiningen.core.project :as p :refer [defproject]]
-  '[leiningen.clean :refer [clean]]
   '[leiningen.install :refer [install]]
   '[leiningen.deploy :refer [deploy]])
 
@@ -15,17 +16,45 @@
                   vals
                   (mapcat :extra-deps)
                   (into deps)
+                  (map (fn parse-coord [coord]
+                         (let [[artifact info] coord
+                               s (str artifact)]
+                           (if-let [i (str/index-of s "$")]
+                             [(symbol (subs s 0 i))
+                              (assoc info :classifier (subs s (inc i)))]
+                             coord))))
                   (reduce
                     (fn [deps [artifact info]]
                       (if-let [version (:mvn/version info)]
                         (conj deps
                           (transduce cat conj [artifact version]
-                            (select-keys info [:scope :exclusions])))
+                            (select-keys info [:exclusions :classifier])))
                         deps))
-                    []))]
+                    []))
+        paths (->> (select-keys aliases aliases-to-include)
+                   vals
+                   (mapcat :extra-paths)
+                   (into paths))]
     {:dependencies deps
      :source-paths []
      :resource-paths paths}))
+
+(defn delete-children-recursively! [f]
+  (when (.isDirectory f)
+    (doseq [f2 (.listFiles f)]
+      (delete-children-recursively! f2)))
+  (when (.exists f) (io/delete-file f)))
+
+(defn build-cljs [file-name opts]
+  (let [out-file (str "resources/public/" file-name ".js")
+        out-dir (str "resources/public/" file-name ".out")]
+    (println "Building" out-file)
+    (delete-children-recursively! (io/file out-dir))
+    (api/build "src" (merge
+                       {:output-to     out-file
+                        :output-dir    out-dir}
+                       opts))
+    (delete-children-recursively! (io/file out-dir))))
 
 (defmulti task first)
 
@@ -38,26 +67,20 @@
 
 (defmethod task "build"
   [_]
-  (-> (read-project-clj) p/init-project clean)
-  (println "Building paren-soup.js")
-  (api/build "src" {:main          'paren-soup.prod
-                    :optimizations :advanced
-                    :output-to     "target-js/public/paren-soup.js"
-                    :output-dir    "target/public/paren-soup.out"})
-  (println "Building paren-soup-compiler.js")
-  (api/build "src" {:main               'paren-soup.compiler
-                    :optimizations      :simple
-                    :output-to          "target-js/public/paren-soup-compiler.js"
-                    :output-dir         "target/public/paren-soup-compiler.out"
-                    :static-fns         true
-                    :optimize-constants true})
-  (println "Building paren-soup-with-compiler.js")
-  (api/build "src" {:main               'paren-soup.core
-                    :optimizations      :simple
-                    :output-to          "target-js/public/paren-soup-with-compiler.js"
-                    :output-dir         "target/public/paren-soup-with-compiler.out"
-                    :static-fns         true
-                    :optimize-constants true}))
+  (-> (read-project-clj) p/init-project)
+  (build-cljs "paren-soup"
+              {:main          'paren-soup.prod
+               :optimizations :advanced})
+  (build-cljs "paren-soup-compiler"
+              {:main               'paren-soup.compiler
+               :optimizations      :simple
+               :static-fns         true
+               :optimize-constants true})
+  (build-cljs "paren-soup-with-compiler"
+              {:main               'paren-soup.core
+               :optimizations      :simple
+               :static-fns         true
+               :optimize-constants true}))
 
 (defmethod task "install"
   [_]
